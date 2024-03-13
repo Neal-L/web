cd6e90b28b5c661e6bd887c26cc4df9e49b772b9
Neal-L <67144366+Neal-L@users.noreply.github.com>
2024-03-13 15:37:46 +0800

    Feat vue
    新增vue 資料夾

---
+++ vue/src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { onMounted,ref } from 'vue';
+
+const count = ref(0);
+const increment = () => {
+  count.value += 1;
+};
+
+onMounted(() => {
+  console.log(count,value);
+});
+</script>
+
+<template>
+  <div>
+    <h1>Vue: {{ count }}</h1>
+    <button @click="increment">Count is: {{ count }}</button>
+  </div>
+</template>
+
+<style>
+</style>
